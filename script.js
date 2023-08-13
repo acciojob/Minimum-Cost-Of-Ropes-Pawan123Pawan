569c4c51745064b8fac1c95e54a04989adf142cd
Pawan123Pawan <105289150+Pawan123Pawan@users.noreply.github.com>
2023-08-13 18:16:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function calculateMinCost() {
+function calculateMinCost(arr) {
   //your code here
-  
-  
-  
+  if (arr.length <= 1) {
+        return 0; // No cost if there's only one or no ropes
+    }
+
+    let totalCost = 0;
+    arr.sort((a, b) => a - b); // Sort the ropes in ascending order
+
+    while (arr.length > 1) {
+        const sum = arr[0] + arr[1]; // Take the two smallest ropes
+        totalCost += sum; // Add their sum to the total cost
+        arr.splice(0, 2); // Remove the two smallest ropes
+        arr.push(sum); // Add the sum back to the ropes
+        arr.sort((a, b) => a - b); // Re-sort the ropes
+    }
+
+    return totalCost;
 }  
